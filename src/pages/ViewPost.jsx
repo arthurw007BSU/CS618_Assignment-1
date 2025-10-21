69c1134b79ffb69722957aaf8f8d9970018a9dc7
arthurw007BSU <arthur.williamson@bsu.edu>
2025-10-21 00:36:54 +0000

    feat: victory for frontend

--- src/pages/ViewPost.jsx
+++ src/pages/ViewPost.jsx
@@ -1,33 +1,25 @@
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react'
 import { useQuery, useMutation } from '@tanstack/react-query'
+import { Helmet } from 'react-helmet-async'
 import { Header } from '../components/Header.jsx'
 import { Post } from '../components/Post.jsx'
+import { PostStats } from '../components/PostStats.jsx'
 import { getPostById } from '../api/posts.js'
-import { Helmet } from 'react-helmet-async'
 import { getUserInfo } from '../api/users.js'
-import { useEffect, useState } from 'react'
 import { postTrackEvent } from '../api/events.js'
 
+function truncate(str, max = 160) {
+  if (!str) return str
+  if (str.length > max) {
+    return str.slice(0, max - 3) + '...'
+  } else {
+    return str
+  }
+}
+
 export function ViewPost({ postId }) {
-  const [session, setSession] = useState()
-
-  const trackEventMutation = useMutation({
-    mutationFn: (action) => postTrackEvent({ postId, action, session }),
-    onSuccess: (data) => setSession(data?.session),
-  })
-
-  useEffect(() => {
-    let timeout = setTimeout(() => {
-      trackEventMutation.mutate('startView')
-      timeout = null
-    }, 1000)
-    return () => {
-      if (timeout) clearTimeout(timeout)
-      else trackEventMutation.mutate('endView')
-    }
-  }, [])
-
   const postQuery = useQuery({
     queryKey: ['post', postId],
     queryFn: () => getPostById(postId),
@@ -41,14 +33,22 @@ export function ViewPost({ postId }) {
   })
   const userInfo = userInfoQuery.data ?? {}
 
-  function truncate(str, max = 160) {
-    if (!str) return str
-    if (str.length > max) {
-      return str.slice(0, max - 3) + '...'
-    } else {
-      return str
+  const [session, setSession] = useState()
+  const trackEventMutation = useMutation({
+    mutationFn: (action) => postTrackEvent({ postId, action, session }),
+    onSuccess: (data) => setSession(data?.session),
+  })
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      trackEventMutation.mutate('startView')
+      timeout = null
+    }, 1000)
+    return () => {
+      if (timeout) clearTimeout(timeout)
+      else trackEventMutation.mutate('endView')
     }
-  }
+  }, [])
+
   return (
     <div style={{ padding: 8 }}>
       {post && (
@@ -60,7 +60,7 @@ export function ViewPost({ postId }) {
           <meta property='og:article:published_time' content={post.createdAt} />
           <meta property='og:article:modified_time' content={post.updatedAt} />
           <meta property='og:article:author' content={userInfo.username} />
-          {(post.tag ?? []).map((tag) => (
+          {(post.tags ?? []).map((tag) => (
             <meta key={tag} property='og:article:tag' content={tag} />
           ))}
         </Helmet>
@@ -71,10 +71,19 @@ export function ViewPost({ postId }) {
       <Link to='/'>Back to main page</Link>
       <br />
       <hr />
-      {post ? <Post {...post} fullPost /> : `Post with id ${postId} not found.`}
+      {post ? (
+        <div>
+          <Post {...post} fullPost />
+          <hr />
+          <PostStats postId={postId} />
+        </div>
+      ) : (
+        `Post with id ${postId} not found.`
+      )}
     </div>
   )
 }
+
 ViewPost.propTypes = {
   postId: PropTypes.string.isRequired,
 }
