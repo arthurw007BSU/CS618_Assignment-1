1289c51fea59cc970279c6b877262e23c326deb4
arthurw007BSU <arthur.williamson@bsu.edu>
2025-10-22 00:12:44 +0000

    feat: mod9 graphsql types users and inputs

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,22 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import { eventRoutes } from './routes/events.js'
 
+import { ApolloServer } from '@apollo/server'
+import { typeDefs, resolvers } from './graphql/index.js'
+import { expressMiddleware } from '@apollo/server/express4'
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
+
+apolloServer
+  .start()
+  .then(() => app.use('/graphql', expressMiddleware(apolloServer)))
 
 postsRoutes(app)
 userRoutes(app)
---
+++ backend/src/graphql/index.js
@@ -0,0 +1,7 @@
+import { querySchema, queryResolver } from './query.js'
+import { postSchema, postResolver } from './post.js'
+import { userSchema, userResolver } from './user.js'
+//import { mutationSchema, mutationResolver } from './mutation.js'
+
+export const typeDefs = [querySchema, postSchema, userSchema]
+export const resolvers = [queryResolver, postResolver, userResolver]
